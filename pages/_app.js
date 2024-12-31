fe0fce79d584a6c24164afd5501f88326e3dd859
brkeae <deividbreake@gmail.com>
2024-12-31 19:19:02 -0300

    Adicionando uma pagina em construção com estilo.

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import '../styles/index.css'; // Importação global do CSS
+import React from 'react';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
-function Home() {
-  return <h1> Página em construção</h1>;
-}
+import React from 'react';
 
-export default Home;
+const Index = () => {
+  return (
+    <div className="under-construction-container">
+      <div className="content">
+        <h1 className="title">Site em Construção</h1>
+        <p className="message">Estamos trabalhando para trazer novidades em breve!</p>
+        <div className="icon">⚡</div>
+      </div>
+    </div>
+  );
+};
+
+export default Index;
