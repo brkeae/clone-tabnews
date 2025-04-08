4721ee64eae6791850cec66a2aed3ff3923ef4b6
brkeae <deividbreake@gmail.com>
2025-04-08 20:40:41 -0300

    ajuste do head

--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,15 @@ import React from "react";
 
 const Index = () => {
   return (
-    <>
-      <Head>
-        <title>Clone TabNews</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className="under-construction-container">
-        <div className="content">
-          <h1 className="title">Site em Construção</h1>
-          <p className="message">
-            Estamos trabalhando para trazer novidades em breve!
-          </p>
-          <div className="icon">⚡</div>
-        </div>
+    <div className="under-construction-container">
+      <div className="content">
+        <h1 className="title">Site em Construção</h1>
+        <p className="message">
+          Estamos trabalhando para trazer novidades em breve!
+        </p>
+        <div className="icon">⚡</div>
       </div>
-    </>
+    </div>
   );
 };
 
