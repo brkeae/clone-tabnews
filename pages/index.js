1fbaf3fbd2066c378c4bf0fd866bc76bf06c7489
brkeae <deividbreake@gmail.com>
2025-04-09 12:16:05 -0300

    incluindo favicon

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,23 @@
+import Head from "next/head";
 import React from "react";
 
 const Index = () => {
   return (
-    <div className="under-construction-container">
-      <div className="content">
-        <h1 className="title">Site em Construção</h1>
-        <p className="message">
-          Estamos trabalhando para trazer novidades em breve!
-        </p>
-        <div className="icon">⚡</div>
+    <>
+      <Head>
+        <title>Clone TabNews</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="under-construction-container">
+        <div className="content">
+          <h1 className="title">Site em Construção</h1>
+          <p className="message">
+            Estamos trabalhando para trazer novidades em breve!
+          </p>
+          <div className="icon">⚡</div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
